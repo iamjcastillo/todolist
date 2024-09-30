82597c13f6eadacda8d64d22c72a5a9723027577
Jesus Castillo <castilloizquierdojesus@gmail.com>
2024-09-30 21:00:10 +0200

    Use repository pattern
    - Useful to handle data from both data sources

---
+++ myapp/infrastructure/repositories/to_do_repository.py
@@ -0,0 +1,27 @@
+from myapp.domain.criteria import Criteria, ToDoCriteria
+from myapp.domain.todo import ToDoList, Task
+from myapp.infrastructure.datasources.tasks_data_source import TasksDataSource
+from myapp.infrastructure.datasources.todo_data_source import ToDoDataSource
+
+
+class ToDoRepository:
+    def __init__(self):
+        self.todo_data_source = ToDoDataSource()
+        self.task_data_source = TasksDataSource()
+
+    def create(self, todo: ToDoList) -> ToDoList:
+        saved_todo = self.todo_data_source.create(todo)
+        saved_tasks = [
+            self.task_data_source.create(
+                Task(id=task.id, todo_id=saved_todo.id, title=task.title, description=task.description, state=task.state)
+            )
+            for task in todo.tasks
+        ]
+
+        return ToDoList(id=saved_todo.id, category=saved_todo.category, tasks=saved_tasks)
+
+    def get(self, criteria: Criteria) -> ToDoList:
+        if isinstance(criteria, ToDoCriteria):
+            return self.todo_data_source.get()
+        else:
+            raise NotImplementedError()
