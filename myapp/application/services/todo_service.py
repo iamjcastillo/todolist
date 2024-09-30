f73ce825525c16241ae82e09499d1ddec578250c
Jesus Castillo <castilloizquierdojesus@gmail.com>
2024-09-30 21:00:45 +0200

    Create service

---
+++ myapp/application/services/todo_service.py
@@ -0,0 +1,25 @@
+from myapp.domain.criteria import Criteria
+from myapp.domain.todo import ToDoList, ToDoListCreationRequest, Task
+from myapp.infrastructure.repositories.to_do_repository import ToDoRepository
+
+
+class ToDoService:
+    def __init__(self):
+        self.todo_repository = ToDoRepository()
+
+    def create(self, request: ToDoListCreationRequest) -> ToDoList:
+        todo_list = ToDoList.create(category=request.category)
+
+        for task_data in request.tasks:
+            task = Task.create(
+                title=task_data.title,
+                description=task_data.description,
+                state=task_data.state,
+                todo_id=todo_list.id,
+            )
+            todo_list.add_task(task)
+
+        return self.todo_repository.create(todo_list)
+
+    def get(self, criteria: Criteria) -> ToDoList:
+        return self.todo_repository.get(criteria)
