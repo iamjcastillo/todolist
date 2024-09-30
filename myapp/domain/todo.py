7030b8a83a6a9cd274aeabbef6f358056d3d4ea7
Jesus Castillo <castilloizquierdojesus@gmail.com>
2024-09-30 20:46:20 +0200

    Create domain

---
+++ myapp/domain/todo.py
@@ -0,0 +1,54 @@
+from typing import NewType, List, Optional
+
+from pydantic import BaseModel
+
+Title = NewType("Title", str)  # This could be a class with a create method
+Description = NewType("Description", str)
+State = NewType("State", str)
+TaskID = NewType("TaskID", int)
+Category = NewType("Category", str)
+ToDoID = NewType("ToDoId", int)
+
+
+# class State(Enum):
+#    PENDING: "PENDING"
+#    COMPLETED: "COMPLETED"
+
+
+class Task(BaseModel):
+    id: Optional[TaskID]
+    title: Title  # Improvement check length
+    description: Description
+    state: State  # Improvement: Use enum
+    todo_id: Optional[ToDoID]
+
+    @classmethod
+    def create(cls, title: Title, description: Description, state: State, todo_id: ToDoID) -> 'Task':
+        return cls(id=None, title=title, description=description, state=state, todo_id=todo_id)
+
+    def mark_as_completed(self):
+        self.state = "COMPLETED"  # Improvement. Use enum here
+
+
+class ToDoList(BaseModel):
+    id: Optional[ToDoID]  # Improvement. Remove nullability
+    tasks: List[Task]
+    category: Category
+
+    @classmethod
+    def create(cls, category: Category) -> 'ToDoList':
+        return cls(id=None, category=category, tasks=[])
+
+    def add_task(self, task: Task):
+        self.tasks.append(task)
+
+
+class TaskCreationRequest(BaseModel):
+    title: Title  # Improvement check length
+    description: Description
+    state: State  # Improvement: Use enum
+
+
+class ToDoListCreationRequest(BaseModel):
+    tasks: List[TaskCreationRequest]
+    category: Category
