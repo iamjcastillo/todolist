b764173e33bd470f840d0bfb19f4c99078e3490f
Jesus Castillo <castilloizquierdojesus@gmail.com>
2024-10-03 20:31:02 +0200

    Open new endpoint to allow title update

--- myapp/infrastructure/routers/todo_routers.py
+++ myapp/infrastructure/routers/todo_routers.py
@@ -3,10 +3,11 @@ from typing import List, TypeVar, Generic
 from fastapi import APIRouter
 from pydantic import BaseModel
 
-from myapp.application.commands.command_handler import DeleteTask, GetToDoList, CreateToDoList
+from myapp.application.commands.command_handler import DeleteTask, GetToDoList, CreateToDoList, UpdateTask
 from myapp.application.services.todo_service import ToDoService
 from myapp.domain.kernel import ToDoID
 from myapp.domain.task import TaskID
+from myapp.domain.title import Title
 from myapp.domain.todo import ToDoList
 from myapp.infrastructure.db.database import DBSessionDependency, db_session
 
@@ -40,3 +41,9 @@ async def get_todo_list(id: ToDoID, db: DBSessionDependency) -> ToDoList:
 async def delete_task(id: ToDoID, task_id: TaskID, db: DBSessionDependency) -> ToDoList:
     db_session.set(db)
     return ToDoService().execute(command=DeleteTask(task_id=task_id, todo_id=id))
+
+
+@router.patch("/lists/{id}/tasks/{task-id}", tags=["Tasks"])
+async def update_task(id: ToDoID, task_id: TaskID, title: str, db: DBSessionDependency) -> ToDoList:
+    db_session.set(db)
+    return ToDoService().execute(command=UpdateTask(task_id=task_id, todo_id=id, title=Title(title)))
