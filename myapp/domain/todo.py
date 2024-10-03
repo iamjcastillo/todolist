24d4561db753962c2fdb8292b87c52a9d60b4607
Jesus Castillo <castilloizquierdojesus@gmail.com>
2024-10-03 20:25:31 +0200

    Include new business rule

--- myapp/domain/todo.py
+++ myapp/domain/todo.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel
 
 from myapp.domain.kernel import ToDoID
 from myapp.domain.task import Task, TaskID
+from myapp.domain.title import Title
 
 Category = NewType("Category", str)
 
@@ -13,6 +14,7 @@ class ToDoList(BaseModel):
     tasks: List[Task]
     category: Category
     _removed_tasks: List[Task] = []
+    _updated_tasks: List[Task] = []
 
     def add_task(self, task: Task):
         self.tasks.append(task)
@@ -26,6 +28,15 @@ class ToDoList(BaseModel):
 
     def get_removed_tasks(self) -> List[Task]:
         return self._removed_tasks
+
+    def update_task(self, task_id: TaskID, title: Title):
+        for task in self.tasks:
+            if task.id == task_id:
+                task.update_title(title)
+                self._updated_tasks.append(task)
+
+    def get_updated_tasks(self) -> List[Task]:
+        return self._updated_tasks
 
 
 class ToDoListFactory:
